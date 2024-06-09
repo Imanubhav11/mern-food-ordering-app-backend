774902f4742baeac0d899bbdd995df26665385fe
Anubhav Das <anubhavdas@Anubhavs-MacBook-Air.local>
2024-06-09 18:46:35 +0530

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import express ,{Request, Response}  from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from './routes/myUserRoute'
+import myUserRoute from './routes/myUserRoute';
+import { v2 as cloudinary } from 'cloudinary';
+import MyRestaurantRoute from './routes/MyRestaurantRoute';
+
 mongoose
-.connect(process.env.MONGODB_CONNECTION_STRING as string)
-.then(() => console.log("Connected to database"));
+    .connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log("Connected to database"));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 app.use(express.json());
@@ -16,8 +25,11 @@ app.get("/health",async(req:Request,res:Response) => {
 });
 
 
-
 app.use("/api/my/user",myUserRoute);
+
+app.use("/api/my/restaurant", MyRestaurantRoute);
+
+
 
 app.listen(5090, () => {
     console.log("Server started on port 5090");
---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import multer from 'multer';
+import MyRestaurantController from '../controller/MyRestaurantController';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyRestaurantRequest } from '../middleware/validation';
+
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+//GET api/my/restaurant
+router.get('/', jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
+// POST request -> api/my/restaurant
+router.post(
+    "/",
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    MyRestaurantController.createMyRestaurant
+  );
+
+
+router.put(
+    "/", 
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtParse,
+    jwtCheck,
+    MyRestaurantController.updateMyRestaurant
+);
+
+
+export default router;
